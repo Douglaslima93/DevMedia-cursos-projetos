db08c139e6c0a1eacf79f7cb7a034e2f66b0d8e7
Douglas Lima <douglas07a@gmail.com>
2023-07-31 11:39:27 -0300

    Novo conteudo

--- api/index1.js
+++ api/index1.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.json({mensagem: 'Hello World 2023'});
 });
 
 app.listen(8080, () => {
