1ce63226eecaae1d70d10135f784b46846c5986c
Douglas Lima <douglas07a@gmail.com>
2023-08-01 12:44:09 -0300

    Exercicios Node.js

---
+++ Node/exercicios/horas.js
@@ -0,0 +1,8 @@
+let data = new Date();
+let hora = data.getHours();
+
+if (hora < 9 || hora >18) {
+    console.log('Loja fechada');
+} else {
+    console.log('Loja Aberta');
+}
---
+++ Node/exercicios/semestre_atual.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+let data = new Date();
+let mes_atual = data.getMonth();
+
+let semestre_atual;
+
+if (mes_atual <= 5) {
+    semestre_atual = {semestre: 'Primeiro semestre'};
+} else {
+    semestre_atual = {semestre: 'Segundo semestre'};
+};
+
+app.get('/', (req,res) => {
+    res.json(semestre_atual);
+});
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado ' + semestre_atual);
+});
