e6735ed6dc09f66d3feff076dd57f4ba11ee96d8
Douglas Lima <121909515+Douglaslima93@users.noreply.github.com>
2023-08-14 21:55:17 -0300

    Atualizar o exercicio2.js

--- JavaScript/filter/exercicio2.js
+++ JavaScript/filter/exercicio2.js
@@ -2,7 +2,9 @@ const carros = [
     { marca: "Fiat", modelo: "Uno", anoFabricacao: 2015},
     { marca: "GM", modelo: "Onix", anoFabricacao: 2018},
     { marca: "Ford", modelo: "KA+", anoFabricacao: 2018},
-    { marca: "VW", modelo: "UP", anoFabricacao: 2022}
+    { marca: "VW", modelo: "UP", anoFabricacao: 2022},
+    { marca:"Nissan", modelo: "Sentra",
+anoFabricacao: 2013}
 ];
 
 function verificaMarca(carro){
