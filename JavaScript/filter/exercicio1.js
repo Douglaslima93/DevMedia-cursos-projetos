98afa5495d2128ca8a80dcb6d095f194a4c515ce
Douglas Lima <121909515+Douglaslima93@users.noreply.github.com>
2023-08-15 20:29:17 -0300

    Atualizar o exercicio1.js

--- JavaScript/filter/exercicio1.js
+++ JavaScript/filter/exercicio1.js
@@ -2,7 +2,9 @@ const produtos = [
     { id: 1, nome:"Açucar", ativo: true, valor: 2.00},
     { id: 2, nome: "Sal", ativo: false, valor: 1.50},
     { id: 3, nome: "Farinha", ativo: false, valor: 4.00},
-    { id: 4, nome: "Feijão", ativo: true, valor: 7.00}
+    { id: 4, nome: "Feijão", ativo: true, valor: 7.00},
+    { id: 5, nome: "Sal", ativo: true,
+valor: 6.00}
 ];
 
 function verificaProdutoAtivo(produto) {
