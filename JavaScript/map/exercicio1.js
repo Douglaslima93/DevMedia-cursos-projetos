07380dcdef1fed49c9063d7dcbecc6617fe90894
Douglas Lima <121909515+Douglaslima93@users.noreply.github.com>
2023-08-16 21:05:27 -0300

    Atualizar o exercicio1.js

--- JavaScript/map/exercicio1.js
+++ JavaScript/map/exercicio1.js
@@ -2,7 +2,9 @@ const carros = [
     { marca: "Fiat", modelo: "Uno", anoFabricacao: 2015},
     { marca: "GM", modelo: "Onix", anoFabricacao: 2018},
     { marca: "Ford", modelo: "KA+", anoFabricacao: 2018},
-    { marca: "VW", modelo: "UP", anoFabricacao: 2022}
+    { marca: "VW", modelo: "UP", anoFabricacao: 2022},
+    { marca: "Nissam", modelo: "Versa", 
+anoFabricacao: 2023}
 ];
 
 function retornaCarro(carro) {
