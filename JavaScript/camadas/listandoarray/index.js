cb75bbf5e3660a1d8632bba6ff94a4bfc5542fee
Douglas Lima <121909515+Douglaslima93@users.noreply.github.com>
2023-08-25 21:45:54 -0300

    Atualizar o index.js

--- JavaScript/camadas/listandoarray/index.js
+++ JavaScript/camadas/listandoarray/index.js
@@ -3,5 +3,7 @@ let listaJogos = [];
 listaJogos.push("Super Mario World");
 listaJogos.push("The Last of Us");
 listaJogos.push("Gears of War");
+listaJogos.push("Mortal kombat");
+
 
 console.log(listaJogos);
